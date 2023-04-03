770a002ac0d13171788df8af02208f6199d6e662
MatheusMeloGTD <matheus.melo@gotodata.com.br>
2023-04-03 08:15:01 -0300

    Teste inicial

--- crawler/page_fetcher.py
+++ crawler/page_fetcher.py
@@ -43,10 +43,18 @@ class PageFetcher(Thread):
         """
         Coleta uma nova URL, obtendo-a do escalonador
         """
-        
+        obj_url, depth = self.obj_scheduler.get_next_url()
+        bin_str_content = self.request_url(obj_url)
+
+        if bin_str_content is not None:
+            print(obj_url.geturl())
+            for obj_new_url, new_depth in self.discover_links(obj_url, depth, bin_str_content):
+                self.obj_scheduler.add_new_page(obj_new_url, new_depth)
 
     def run(self):
         """
         Executa coleta enquanto houver páginas a serem coletadas
         """
-        pass
+        while not self.obj_scheduler.has_finished_crawl():
+            self.crawl_new_url()
+            self.obj_scheduler.count_fetched_page()
